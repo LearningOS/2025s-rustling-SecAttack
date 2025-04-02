ed4e1cf5def919ac4cdc5358b1640af535e4533f
SecAttack <714719425@qq.com>
2025-04-02 16:02:19 +0800

    第二天代码

--- exercises/hashmaps/hashmaps1.rs
+++ exercises/hashmaps/hashmaps1.rs
@@ -11,18 +11,22 @@
 // Execute `rustlings hint hashmaps1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// date:20250402
 
 use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
-    let mut basket = // TODO: declare your hash map here.
+    let mut basket = HashMap::new();// TODO: declare your hash map here.
 
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
 
     // TODO: Put more fruits in your basket here.
 
+    basket.insert(String::from("apple"), 8);
+    basket.insert(String::from("mango"), 3);
+    basket.insert(String::from("cherry"),4);
+    basket.insert(String::from("peach"), 5);
     basket
 }
 
